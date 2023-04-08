ba8fed94d91430a56ca817c8aef11b5858ae92a5
KyouShou <31533013+KyouShou@users.noreply.github.com>
2023-04-09 07:17:15 +0800

    refactor:將測試中的StringCalculator改為私有成員

--- TDD.StringCalculator.Tests/StringCalculatorTests.cs
+++ TDD.StringCalculator.Tests/StringCalculatorTests.cs
@@ -2,16 +2,18 @@ namespace TDD.StringCalculator.Tests
 {
     public class StringCalculatorTests
     {
+        StringCalculator _stringCalculator;
+
         [SetUp]
         public void Setup()
         {
+            _stringCalculator = new StringCalculator();
         }
 
         [Test]
         public void Add_Given_EmptyString_Returns_O()
         {
-            var stringCalculator = new StringCalculator();
-            var actual = stringCalculator.Add(String.Empty);
+            var actual = _stringCalculator.Add(String.Empty);
             Assert.AreEqual("O", actual);
         }
     }
